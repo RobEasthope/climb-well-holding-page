721596f8d1784ff4bea8c348cbcd082db4d3fc9d
Rob Easthope <hello@robeasthope.com>
2024-07-09 09:30:29 +0100

    Add tailwing eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import tailwind from "eslint-plugin-tailwindcss";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,57 +13,69 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 
-export default [{
+export default [
+  {
     ignores: [
-        ".cache",
-        ".astro",
-        ".vercel",
-        "node_modules",
-        "dist",
-        "public",
-        "src/env.d.ts",
+      ".cache",
+      ".astro",
+      ".vercel",
+      "node_modules",
+      "dist",
+      "public",
+      "src/env.d.ts",
     ],
-}, ...compat.extends(
+  },
+  ...compat.extends(
     "@thoughtbot/eslint-config",
     "@thoughtbot/eslint-config/typescript",
     "plugin:astro/recommended",
     "plugin:astro/jsx-a11y-recommended",
-), {
+  ),
+  {
     languageOptions: {
-        ecmaVersion: 5,
-        sourceType: "script",
+      ecmaVersion: 5,
+      sourceType: "script",
 
-        parserOptions: {
-            project: "./tsconfig.json",
-        },
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
     },
 
     rules: {
-        "import/prefer-default-export": "off",
+      "import/prefer-default-export": "off",
 
-        "react/jsx-filename-extension": [1, {
-            extensions: [".ts", ".tsx", ".astro"],
-        }],
+      "react/jsx-filename-extension": [
+        1,
+        {
+          extensions: [".ts", ".tsx", ".astro"],
+        },
+      ],
     },
-}, {
+  },
+  {
     files: ["**/*.astro"],
 
     languageOptions: {
-        parser: parser,
-        ecmaVersion: 5,
-        sourceType: "script",
+      parser: parser,
+      ecmaVersion: 5,
+      sourceType: "script",
 
-        parserOptions: {
-            parser: "@typescript-eslint/parser",
-            extraFileExtensions: [".astro"],
-        },
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
     },
 
     rules: {
-        "react/no-unknown-property": ["error", {
-            ignore: ["class"],
-        }],
+      "react/no-unknown-property": [
+        "error",
+        {
+          ignore: ["class"],
+        },
+      ],
     },
 
     processor: "astro/client-side-ts",
-}];
+  },
+  ...tailwind.configs["flat/recommended"],
+];
